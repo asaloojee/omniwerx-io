57ec976df35a763ee219e8a5eddb9466c8b023b9
asaloojee <asaloojee@omniwerx.io>
2026-08-11 00:57:00 +0100

    dropped sharp and fixed image in about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://omniwerx.io",
+  image: {
+    service: passthroughImageService(),
+  },
   vite: {
     assetsInclude: ["**/*.glb", "**/*.hdr"],
   },
