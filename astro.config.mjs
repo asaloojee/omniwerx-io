e7973a0b84ffb0348f5dec32c32aff55fce0c38a
asaloojee <asaloojee@omniwerx.io>
2026-08-14 01:06:10 +0100

    CSP rules and about name styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,27 @@ export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
+  security: {
+    csp: {
+      directives: [
+        "default-src 'self'",
+        "base-uri 'none'",
+        "connect-src 'self'",
+        "font-src 'self'",
+        "form-action 'self'",
+        "frame-src 'none'",
+        "img-src 'self'",
+        "object-src 'none'",
+        "worker-src 'self' blob:",
+      ],
+      scriptDirective: {
+        resources: ["'self'", "'wasm-unsafe-eval'"],
+      },
+      styleDirective: {
+        resources: [{ resource: "'unsafe-inline'", kind: "attribute" }],
+      },
+    },
+  },
   vite: {
     assetsInclude: ["**/*.glb", "**/*.hdr"],
   },
